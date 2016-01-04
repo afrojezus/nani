13b76146270b3ae29a7a5b6da85feb6b45f765c5
Juan Soto <juan@juansoto.me>
2016-01-03 21:04:31 -0600

    test(authentication): Add authentication tests

--- test.js
+++ test.js
@@ -2,6 +2,7 @@
 
 import test from 'ava';
 import nani from './';
+import authenticate from './lib/authenticate';
 import {url, isExpired} from './lib/utils';
 
 const id = 'dummytest-hxdu6';
@@ -29,4 +30,18 @@ test('isExpired works', t => {
   t.ok(isExpired(Math.floor(Date.now() / 1000) - 500));
   t.notOk(isExpired(Math.floor(Date.now() / 1000) + 100));
   t.notOk(isExpired(1000000000000));
+});
+
+test('authenticate resolves on good id and secret', async t => {
+  let data = await authenticate(id, secret);
+
+  t.ok(data.hasOwnProperty('token'));
+  t.ok(data.hasOwnProperty('expires'));
+});
+
+test('._authenticate resolves with good id and secret', async t => {
+  let client = nani.init(id, secret);
+  await client._authenticate();
+
+  t.notSame(client.authInfo.token, '');
 });
