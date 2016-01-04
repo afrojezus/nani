a31324f4842def869a995e0f3a1ff1a16c033e45
Juan Soto <juan@juansoto.me>
2016-01-03 21:04:17 -0600

    feat(authentication): Add authentication

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const authenticate = require('./lib/authenticate');
+
 let nani = {
   init: function (id, secret) {
     this.id = id;
@@ -13,6 +15,17 @@ let nani = {
     token: '',
     expires: ''
   }
+};
+
+nani._authenticate = function () {
+  let id = this.id;
+  let secret = this.secret;
+
+  return authenticate(id, secret)
+    .then(data => {
+      this.authInfo.token = data.token;
+      this.authInfo.expires = data.expires;
+    });
 };
 
 module.exports = nani;
